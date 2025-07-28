392101c41f37ba3b50ae54454ea2a73d3940a2bb
akihide-hatano <a.h.19841202@gmail.com>
2025-07-28 22:12:33 +0900

    RestaurantControllerの修正。

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use Illuminate\Support\Facades\DB;
 
 class RestaurantController extends Controller
 {
-    //
+
+    /**
+     * 焼肉店一覧を表示 (検索・絞り込み機能を追加)
+     */
+    public function index(Request $request){
+
+        $query = Restaurant::query();
+    
+        //平均を取得
+        $query->withAvg('reviews','rating');
+
+        //---検索機能---
+        if($request->filled('search')){
+            $search = $request->input('search');
+            $query->where(function ($q) use ($search){
+                $q->where('name', 'like', '%' . $search . '%')
+                ->orWhere('description', 'like', '%' . $search . '%');
+            });
+        }
+    }
 }
