8ca3467bea19b11c43a28ce2c3c666875328f3d6
akihide-hatano <a.h.19841202@gmail.com>
2025-07-28 15:03:05 +0900

    web.phpにてRestaurantControllerのルーティングを追加しました。

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resources('restaurants',[RestaurantController::class]);
+
 require __DIR__.'/auth.php';
