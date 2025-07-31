532213c116221cfc89417eb2c6e5c3ebb58da289
akihide-hatano <a.h.19841202@gmail.com>
2025-08-01 06:17:16 +0900

    headerの配色変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,10 @@
-<nav x-data="{ open: false }" class="bg-gray-800 border-b border-gray-700">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-200" />
+                        <img src="{{ asset('images/yakiniku-logo.png')}}" alt="京都焼肉logo" class="block h-16 w-auto">
                     </a>
                 </div>
 
