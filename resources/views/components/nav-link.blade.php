6e023d261257ea969cd562aee1afce3311f4b082
akihide-hatano <a.h.19841202@gmail.com>
2025-08-01 06:32:01 +0900

    nav-linkを修正。

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-orange-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-orange-700 transition duration-150 ease-in-out'
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-orange-500 text-sm font-medium leading-5 text-gray-900 bg-orange-50 focus:outline-none focus:border-orange-700 transition duration-150 ease-in-out'
             : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-600 hover:text-gray-900 hover:border-gray-300 focus:outline-none focus:text-gray-900 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
