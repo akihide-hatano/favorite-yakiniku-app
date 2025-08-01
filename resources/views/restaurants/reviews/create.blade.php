0151468eb4b2accf7e6e452832d0391b7f361d91
akihide-hatano <a.h.19841202@gmail.com>
2025-08-01 17:16:17 +0900

    buttonのデザインを修正

--- resources/views/restaurants/reviews/create.blade.php
+++ resources/views/restaurants/reviews/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="bg-orange-300 py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h1 class="text-2xl font-bold mb-6 text-center">{{ $restaurant->name }} へのレビューを投稿</h1>
@@ -48,8 +48,9 @@
                         </div>
 
                         <div class="flex items-center justify-between">
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                レビューを投稿する
+                            <button type="submit" class="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center gap-2">
+                                <x-icons.pencil class="size-6 mr-2"/>
+                                <span>レビューを投稿する</span>
                             </button>
                         </div>
                     </form>
