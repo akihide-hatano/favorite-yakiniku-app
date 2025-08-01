593dac13d0e69f8bc6efdfc8317a3a005373b4c9
akihide-hatano <a.h.19841202@gmail.com>
2025-08-01 13:29:48 +0900

    restaurants/index.blade.phpの修正

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -19,7 +19,7 @@
                         <input type="text" name="search" placeholder="店舗名やキーワードで検索"
                             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full mb-2"
                             value="{{ request('search') }}">
-                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                             <x-icons.search/>
                             <span class="ml-2">検索</span>
                         </button>
@@ -79,9 +79,10 @@
                         </form>
                     </div>
                     {{-- 焼肉店の登録 --}}
-                    <div class="mt-6 pt-4 border-t border-gray-200"> {{-- 上部に線と余白を追加して区切る --}}
-                        <a href="{{ route('restaurants.create') }}" class="w-full inline-flex justify-center items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                            新しい焼肉店を登録
+                    <div class="mt-6 py-2 border-t border-gray-200"> {{-- 上部に線と余白を追加して区切る --}}
+                        <a href="{{ route('restaurants.create') }}" class="w-full inline-flex justify-center items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                            <x-icons.pencil class=""/>
+                            <span class="ml-1">新しい焼肉店を登録</span>
                         </a>
                     </div>
                 </div>
