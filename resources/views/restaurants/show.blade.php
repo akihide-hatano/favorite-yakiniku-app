90df49b7af63b48bb3e29f4c7aa90e407a5807e2
akihide-hatano <a.h.19841202@gmail.com>
2025-08-04 10:08:59 +0900

    restaurants/index.blade.phpのlayoutを修正

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -4,7 +4,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900">
-            {{-- flucshメッセージ --}}
+            {{-- flushメッセージ --}}
             @if (session('status'))
                 <div class="mb-4 font-medium text-sm text-green-600 bg-green-100 p-4 rounded-md">
                     {{ session('status') }}
